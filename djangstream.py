0d3d9804abf04029325989f4200e47a320f31c64
Djangogsheets <126946212+Djangogsheets@users.noreply.github.com>
2024-11-15 11:16:17 +0200

    csv changed and 2024-11-15 11:16:16

--- djangstream.py
+++ djangstream.py
@@ -19,17 +19,17 @@ if os.path.exists(file):
     st.dataframe(df)
 
 
-    //Define variables for input elements
+    #//Define variables for input elements
 var fieldEl = document.getElementById("filter-field");
 var typeEl = document.getElementById("filter-type");
 var valueEl = document.getElementById("filter-value");
 
-//Custom filter example
+#//Custom filter example
 function customFilter(data){
     return data.car && data.rating < 3;
 }
 
-//Trigger setFilter function with correct parameters
+#//Trigger setFilter function with correct parameters
 function updateFilter(){
   var filterVal = fieldEl.options[fieldEl.selectedIndex].value;
   var typeVal = typeEl.options[typeEl.selectedIndex].value;
@@ -49,12 +49,12 @@ function updateFilter(){
   }
 }
 
-//Update filters on value change
+#//Update filters on value change
 document.getElementById("filter-field").addEventListener("change", updateFilter);
 document.getElementById("filter-type").addEventListener("change", updateFilter);
 document.getElementById("filter-value").addEventListener("keyup", updateFilter);
 
-//Clear filters on "Clear Filters" button click
+#//Clear filters on "Clear Filters" button click
 document.getElementById("filter-clear").addEventListener("click", function(){
   fieldEl.value = "";
   typeEl.value = "=";
