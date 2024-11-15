168dd44ada4db663d938014e17b3ae1790c154f4
Djangogsheets <126946212+Djangogsheets@users.noreply.github.com>
2024-11-15 11:29:44 +0200

    csv changed and 2024-11-15 11:29:44

--- djangstream.py
+++ djangstream.py
@@ -26,6 +26,78 @@ if os.path.exists(file):
         for col in columns:
             column_setting.append(f"""{{"title":"{col}", "field":"{col}", "width":200, "sorter":"string", "hozAlign":"center", "headerFilter":"input", "editor": "input"}}""")
 
+        # custom max min filter function
+        var minMaxFilterFunction = function(headerValue, rowValue, rowData, filterParams){
+            //headerValue - the value of the header filter element
+            //rowValue - the value of the column in this row
+            //rowData - the data for the row being filtered
+            //filterParams - params object passed to the headerFilterFuncParams property
+
+            if(rowValue){
+                if(headerValue.start != ""){
+                    if(headerValue.end != ""){
+                        return rowValue >= headerValue.start && rowValue <= headerValue.end;
+                    }else{
+                        return rowValue >= headerValue.start;
+                    }
+                }else{
+                    if(headerValue.end != ""){
+                        return rowValue <= headerValue.end;
+                    }
+                }
+            }
+            return true; //must return a boolean, true if it passes the filter.
+        }
+
+        # custom max min header filter
+        var minMaxFilterEditor = function(cell, onRendered, success, cancel, editorParams){
+            var end;
+            var container = document.createElement("span");
+            //create and style inputs
+            var start = document.createElement("input");
+            start.setAttribute("type", "number");
+            start.setAttribute("placeholder", "Min");
+            start.setAttribute("min", 0);
+            start.setAttribute("max", 100);
+            start.style.padding = "4px";
+            start.style.width = "50%";
+            start.style.boxSizing = "border-box";
+            start.value = cell.getValue();
+
+            function buildValues(){
+                success({
+                    start:start.value,
+                    end:end.value,
+                });
+            }
+
+            function keypress(e){
+                if(e.keyCode == 13){
+                    buildValues();
+                }
+
+                if(e.keyCode == 27){
+                    cancel();
+                }
+            }
+
+            end = start.cloneNode();
+            end.setAttribute("placeholder", "Max");
+
+            start.addEventListener("change", buildValues);
+            start.addEventListener("blur", buildValues);
+            start.addEventListener("keydown", keypress);
+
+            end.addEventListener("change", buildValues);
+            end.addEventListener("blur", buildValues);
+            end.addEventListener("keydown", keypress);
+
+            container.appendChild(start);
+            container.appendChild(end);
+
+            return container;
+        }
+
         components.html(f"""
         <!DOCTYPE html>
         <html lang="en">
@@ -53,6 +125,9 @@ if os.path.exists(file):
                     tooltips: true,
                     columns: [{', '.join(column_setting)}],
                     
+                    headerFilterFunc:minMaxFilterFunction,
+                    headerFilterLiveFilter:false,
+                    headerFilterEditor:minMaxFilterEditor,
                 }});
             </script>
         </body>
@@ -64,4 +139,5 @@ if os.path.exists(file):
 else:
     st.title("Error")
     st.write(f"The file '{file}' does not exist. Please ensure it is located in the correct directory.")
+
 
