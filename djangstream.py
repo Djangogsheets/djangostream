81e59daf007f96cbfe34ca3b23078a55336e10cc
Djangogsheets <126946212+Djangogsheets@users.noreply.github.com>
2024-11-15 10:52:27 +0200

    csv changed and 2024-11-15 10:52:26

--- djangstream.py
+++ djangstream.py
@@ -61,7 +61,7 @@ if os.path.exists(file):
                     {title:card, field:card, width:200, sorter:"string", hozAlign:"center", headerFilter:"input", editor: "input", topCalc:"sum"},                            
                     {title:POS_date, field:date, width:200, sorter:"string", hozAlign:"center", headerFilter:"input", editor: "input"},
                     {title:POS_number, field:posnumber, width:200, sorter:"string", hozAlign:"center", headerFilter:"input", editor: "input"},      
-                    {title:fk_user_valid, field:user_valid, width:200, sorter:"string", hozAlign:"center", headerFilter:"input", editor: "input"},}}
+                    {title:fk_user_valid, field:user_valid, width:200, sorter:"string", hozAlign:"center", headerFilter:"input", editor: "input"},]
                 }});
             </script>
         </body>
